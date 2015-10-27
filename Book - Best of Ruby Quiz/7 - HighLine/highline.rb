f4913eed0f54a3681ec51c8a861ab59bf0804103
Marvin Seguna <mseguna1@gmail.com>
2015-10-28 00:45:10 +0100

    Updated test cases to input String instead of Fixnum

--- Book - Best of Ruby Quiz/7 - HighLine/highline.rb
+++ Book - Best of Ruby Quiz/7 - HighLine/highline.rb
@@ -10,8 +10,12 @@ class Highline
 		end
 	end
 	
-	def self.handle_integer( response, filters )
-		return true
+	def handle_integer( response, filters = nil )
+		raise ArgumentError, "Value [#{response}] is not of type integer!" if ( response =~ /\A[-+]?[0-9]+\z/ ) == nil
+		raise ArgumentError, "Value does not fall within range [#{response}]!" if filters.class == Hash and filters.keys.include? :within and !filters[:within].cover? response.to_i
+		raise ArgumentError, "Value [#{response}] is not #{filters.to_s}!" if filters != nil and ['odd', 'even'].include? filters.to_s and !response.to_i.send(filters.to_s + "?")
+		
+		true
 	end
 	
 end
--- Book - Best of Ruby Quiz/7 - HighLine/test_highline.rb
+++ Book - Best of Ruby Quiz/7 - HighLine/test_highline.rb
@@ -10,52 +10,58 @@ class TestHighline < Test::Unit::TestCase
 	######### INTEGER TESTING ############
 	
 	def test_integer_class
-		assert_equal true, @highline.handle_integer( 2 )
+		assert_equal true, @highline.handle_integer( '2' )
 	
-		assert_raise_with_message( ArgumentError, 'Value is not of type integer!' ) do
-			@highline.handle_integer 'ruby'
+		value = '999t'
+		assert_raise( ArgumentError, "Value [#{value}] is not of type integer!" ) do
+			@highline.handle_integer value
 		end
 	end
  
 	def test_integer_range
-		assert_equal true, @highline.handle_integer( 2, :within => 1..5  )
+		assert_equal true, @highline.handle_integer( '2', :within => 1..5  )
 		
-		assert_raise_with_message( ArgumentError, 'Value does not fall within specified range!' ) do
-			@highline.handle_integer 2, :within => 3..6
+		value = 3..6
+		assert_raise( ArgumentError, "Value does not fall within range [#{value}]!" ) do
+			@highline.handle_integer '2', :within => 3..6
 		end
 	end
 	
 	def test_integer_even
-		assert_equal true, @highline.handle_integer( 2, :even )
+		assert_equal true, @highline.handle_integer( '2', :even )
 		
-		assert_raise_with_message( ArgumentError, 'Value is not even!' ) do
-			@highline.handle_integer 3, :even
+		value = '3'
+		assert_raise( ArgumentError, "Value [#{value}] is not even!" ) do
+			@highline.handle_integer value, :even
 		end
 	end
 	
 	def test_integer_odd
-		assert_equal true, @highline.handle_integer( 2, :odd )
+		assert_equal true, @highline.handle_integer( '3', :odd )
 		
-		assert_raise_with_message( ArgumentError, 'Value is not odd!' ) do
-			@highline.handle_integer 3, :odd
+		value = '2'
+		assert_raise( ArgumentError, "Value [#{value}] is not odd!" ) do
+			@highline.handle_integer value, :odd
 		end
 	end
 	
 	def test_integer_greater
-		assert_equal true, @highline.handle_integer( 2, :greater => 1 )
+		assert_equal true, @highline.handle_integer( '2', :greater => 1 )
 		
-		value = 5
-		assert_raise_with_message( ArgumentError, "Value must be greater than #{value}!" ) do
-			@highline.handle_integer 3, :greater => value
+		value = '3'
+		greater_value = 5
+		assert_raise( ArgumentError, "Value [#{value}] must be greater than #{greater_value}!" ) do
+			@highline.handle_integer value, :greater => greater_value
 		end
 	end
 	
 	def test_integer_less
-		assert_equal true, @highline.handle_integer( 2, :less => 3 )
+		assert_equal true, @highline.handle_integer( '2', :less => 3 )
 		
-		vale = 1
-		assert_raise_with_message( ArgumentError, "Value must be smaller than #{value}!" ) do
-			@highline.handle_integer 3, :less => value
+		value = '3'
+		greater_value = 1
+		assert_raise( ArgumentError, "Value [#{value}] must be smaller than #{greater_value}!" ) do
+			@highline.handle_integer value, :smaller => greater_value
 		end
 	end
  
