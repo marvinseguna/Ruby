12e663b98a522f2557a3e1ae473bd699b9e6986a
Marvin Seguna <mseguna1@gmail.com>
2015-11-03 02:14:48 +0100

    SIMPLIFIED method

--- Book - Best of Ruby Quiz/8 - Roman Numerals/main.rb
+++ Book - Best of Ruby Quiz/8 - Roman Numerals/main.rb
@@ -1,70 +1,12 @@
-#NOT FINISHED
+ROMAN_LETTERS = { 'I' => 1, 'IV' => 4, 'V' => 5, 'IX' => 9, 'X' => 10, 'XL' => 40, 'L' => 50, 'XC' => 90, 'C' => 100, 'CD' => 400, 'D' => 500, 'CM'  => 900, 'M' => 1000 }
 
-ROMAN_LETTERS = { :I => 1, :V => 5, :X => 10, :L => 50, :C => 100, :D => 500, :M => 1000 }
-
-class String
-	def is_not_an_integer
-		( self =~ /^-?\d*$/ ) == nil
-	end
-end
-
-def get_splitted_value( number ) #Return: array
-	number = number.to_s
-	raise ArgumentError, 'Invalid argument [get_splitted_value]'		if number.is_not_an_integer
-	
-	splitted_values = []
-	for i in 0..number.length - 1
-		splitted_values.push number[i] + ( '0' * ( number.length - 1 - i )) # 256 => ['200', '50', '6']
-	end
-	splitted_values
-end
-
-def get_roman_value( arabic )
-	raise ArgumentError, 'Invalid argument [get_roman_value]'		if arabic.to_s.is_not_an_integer
-	arabic = arabic.to_i
-	raise ArgumentError, 'Invalid argument [get_roman_value]'		if arabic <= 0
-	multiple = '1' +  '0' * ( arabic.to_s.length - 1 )
-	raise ArgumentError, 'Invalid argument [get_roman_value]'		if arabic % multiple.to_i != 0
-	
-	roman_keys = ROMAN_LETTERS.keys.reverse
-	roman_values = ROMAN_LETTERS.values
-	index = 0
-	roman = ''
-	while arabic > 0
-		if roman_values.include? arabic
-			ROMAN_LETTERS.each{ |key, value| roman << key.to_s		if value == arabic }
-			arabic = 0
-			next
-		end
-	
-		arabic_clone = arabic - ROMAN_LETTERS[roman_keys[index]]
-		reduction = reduction_check arabic_clone, roman_keys		if arabic_clone < 0
-	
-		if reduction.to_s != ''
-			roman << ( reduction + roman_keys[index].to_s )
-			arabic = 0
-		elsif arabic_clone < 0
-			index = index + 1
-		elsif arabic_clone >= 0
-			arabic = arabic_clone
-			roman << roman_keys[index].to_s
-		end
-	end
-	roman
-end
-
-def reduction_check( arabic, roman_letters )
-	raise ArgumentError, 'Invalid argument [reduction_check]'		if arabic.to_s.is_not_an_integer
-	raise ArgumentError, 'Invalid argument [reduction_check]'		if arabic.to_i >= 0
-	
-	roman_letters.each{ |letter|
-		return letter.to_s		if arabic + ROMAN_LETTERS[letter] == 0
+def arabic_to_roman( arabic, roman_s = '' )
+	raise ArgumentError, 'Invalid argument [arabic_to_roman]'		if arabic.class != Fixnum
+	ROMAN_LETTERS.keys.reverse.each{ |roman| 
+		div = arabic.divmod ROMAN_LETTERS[roman]
+		next		if div[0] == 0
+		roman_s << ( roman * div[0] )
+		arabic = div[1]
 	}
-	return ''
+	roman_s
 end
-
-roman = ''
-get_splitted_value( 4999 ).each{ |value|
-	roman << get_roman_value( value )
-}
-puts roman
--- Book - Best of Ruby Quiz/8 - Roman Numerals/test_AtoR_main.rb
+++ Book - Best of Ruby Quiz/8 - Roman Numerals/test_AtoR_main.rb
@@ -1,76 +1,25 @@
 require 'rspec'
 require 'main'
 
-describe '#get_splitted_value' do
+describe '#arabic_to_roman' do
 
 	context 'is passed a valid value' do
-		it 'is a multiple of 100' do
-			expect( get_splitted_value 200 ).to eq ['200', '00', '0']
+		it 'is passed a vlaue < 100' do
+			expect( arabic_to_roman 78 ).to eq 'LXXVIII'
 		end
 		
-		it 'is not a multiple of 100' do
-			expect( get_splitted_value '394' ).to eq ['300', '90', '4']
+		it 'is passed a vlaue < 1000' do
+			expect( arabic_to_roman 343 ).to eq 'CCCXLIII'
+		end
+		
+		it 'is passed a vlaue < 3999' do
+			expect( arabic_to_roman 2165 ).to eq 'MMCLXV'
 		end
 	end
 	
-	context 'not passed a valid value' do
-		it 'contains letters and values' do
-			expect{ get_splitted_value 'th65f' }.to raise_error(ArgumentError, 'Invalid argument [get_splitted_value]')
-		end
-	end
-end
-
-describe '#get_roman_value' do
-
-	context 'is passed a valid value' do
-		it 'is a multiple of 100' do
-			expect( get_roman_value 200 ).to eq 'CC'
-		end
-		
-		it 'is passed value [90]' do # 4, 9 both require extra method
-			expect( get_roman_value '90' ).to eq 'XC'
-		end
-	end
-
-	context 'not passed a valid value' do
-		it 'is not a multiple of 10/100' do
-			expect{ get_roman_value 213 }.to raise_error(ArgumentError, 'Invalid argument [get_roman_value]')
-		end
-		
-		it 'contains letters and values' do
-			expect{ get_roman_value 'th65f' }.to raise_error(ArgumentError, 'Invalid argument [get_roman_value]')
-		end
-		
-		it 'iss less than 1' do
-			expect{ get_roman_value '0' }.to raise_error(ArgumentError, 'Invalid argument [get_roman_value]')
-		end
-	end
-end
-
-describe '#reduction_check' do #valid value for this function should be < 0!
-	let(:roman_letters) { [:M, :D, :C, :L, :X, :V, :I] } #roman letters - reversed
-	
-	context 'is passed a valid value' do
-		it 'is passed a value which does not match' do
-			expect( reduction_check -9, roman_letters ).to eq ''
-		end
-		
-		it 'is passed a value less than 0 but does not match' do
-			expect( reduction_check -10, roman_letters ).to eq 'X'
-		end
-	end
-
 	context 'is passed an invalid value' do
-		it 'is passed a value greeater than 0' do
-			expect{ reduction_check '100', roman_letters }.to raise_error(ArgumentError, 'Invalid argument [reduction_check]')
-		end
-		
-		it 'is passed a value [0]' do
-			expect{ reduction_check 0, roman_letters }.to raise_error(ArgumentError, 'Invalid argument [reduction_check]')
-		end
-		
-		it 'contains letters and values' do
-			expect{ reduction_check 'th65f', roman_letters }.to raise_error(ArgumentError, 'Invalid argument [reduction_check]')
+		it 'is of type String' do
+			expect{ arabic_to_roman '100' }.to raise_error(ArgumentError, 'Invalid argument [arabic_to_roman]')
 		end
 	end 
 end
