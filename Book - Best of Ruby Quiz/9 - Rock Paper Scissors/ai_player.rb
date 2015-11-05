23fde3e7ace6211aaa7aac6234af98f1869a6ad6
Marvin Seguna <mseguna1@gmail.com>
2015-11-05 23:49:26 +0100

    2nd Commit - Incorporated some AI - NOT FINISHED YET

--- Book - Best of Ruby Quiz/9 - Rock Paper Scissors/ai_player.rb
+++ Book - Best of Ruby Quiz/9 - Rock Paper Scissors/ai_player.rb
@@ -4,31 +4,92 @@ class YourPlayer < Player
 		# opponent_name = String of opponent' s class name
 		#
 		# Player' s constructor sets @opponent_name
-		@irrispective = []
+		@opp_last_option = nil
+		@my_last_option = nil
+		@round = 0
+		@result = nil
 		@opp_los = {'11' => 0, '12' => 0, '13' => 0, '21' => 0, '22' => 0, '23' => 0, '31' => 0, '32' => 0, '33' => 0}
 		@opp_tie = {'11' => 0, '12' => 0, '13' => 0, '21' => 0, '22' => 0, '23' => 0, '31' => 0, '32' => 0, '33' => 0}
 		@opp_win = {'11' => 0, '12' => 0, '13' => 0, '21' => 0, '22' => 0, '23' => 0, '31' => 0, '32' => 0, '33' => 0}
 	end
 	def choose
-		# (required) return your choice of :paper, :rock or :scissors
+		return :rock		if @result == nil #cause I'm a man :D
+
+		first_possibility =  first_strategy
+		return first_possibility		if @round < 50
+		
+		second_possibility = second_strategy
+		
+		if @round < 300
+			if first_possibility != second_possibility
+				rand = @round % 2
+				rand ? ( return first_possibility ) : ( return second_possibility )
+			end
+		else
+			return second_possibility if first_possibility != second_possibility
+		end
+		return first_possibility
 	end
 
 	def result( your_choice, opponents_choice, win_lose_or_draw )
 		# (optional) called after each choice you make to give feedback
 		# your_choice = your choice
-		# oppenents_choice = opponent' s choice
+		# opponents_choice = opponent' s choice
 		# win_lose_or_draw = :win, :lose or :draw, your result
 		
-		add_pattern win_lose_or_draw, oppenents_choice		if !irrispective.empty?
-		@irrispective.push oppenents_choice
+		@result = win_lose_or_draw
+		add_pattern opponents_choice		if @opp_last_option != nil
+		@opp_last_option = opponents_choice
+		@my_last_option = your_choice
+		@round += 1
 	end
 	
-	def add_pattern( win_lose_or_draw, opponents_choice )
-		joint_possibility = hand_to_number @irrispective.last, opponents_choice
+	def second_strategy
+		mappings = { :rock => '1', :paper => '2', :scissors => '3'}
+		val_map = mappings[@my_last_option]
+		final = {}
 		
-		if win_lose_or_draw == :win #Opponent lost
+		case @result
+		when :win
+			@opp_win.each{ |key, value|	final[key] = value if key.start_with? val_map }
+		when :lose
+			@opp_los.each{ |key, value|	final[key] = value if key.start_with? val_map }
+		when :draw
+			@opp_tie.each{ |key, value|	final[key] = value if key.start_with? val_map }
+		end
+		puts @opp_los
+		puts @opp_tie
+		puts @opp_win
+		number_hand = final.sort_by{ |key, value| value }.reverse.first.first[1]
+		mappings.key number_hand
+	end
+	
+	def first_strategy
+		if @result == :won
+			return cycle		if @round % 2 == 0
+			return @my_last_option
+		else #tie/lost
+			return cycle
+		end
+	end
+	
+	def cycle
+		case @my_last_option
+		when :rock
+			return :paper
+		when :paper
+			return :scissors
+		when :scissors
+			return :rock
+		end
+	end
+	
+	def add_pattern( opponents_choice )
+		joint_possibility = hand_to_number @opp_last_option, opponents_choice
+		
+		if @result == :win #Opponent lost
 			@opp_los[ joint_possibility ] += 1
-		elsif win_lose_or_draw == :lose #Opponent won
+		elsif @result == :lose #Opponent won
 			@opp_win[ joint_possibility ] += 1
 		else#tie
 			@opp_tie[ joint_possibility ] += 1
