9634ea7f150f46e215312686608eb4031d05ce8b
Marvin Seguna <mseguna1@gmail.com>
2015-11-10 00:11:03 +0100

    Working version - without prohibited. Needs code refactoring.

--- Book - Best of Ruby Quiz/10 - Knight's Travails/main.rb
+++ Book - Best of Ruby Quiz/10 - Knight's Travails/main.rb
@@ -3,10 +3,16 @@
 #prohibited locations
 
 MAPS_LETTERS = { 'A' => 0, 'B' => 1, 'C' => 2, 'D' => 3, 'E' => 4, 'F' => 5, 'G' => 6, 'H' => 7 }
-MAPS_NUMBERS = { '8' => 0, '7' => 1, '6' => 2, '5' => 3, '4' => 4, '3' => 5, '2' => 6, '1' => 7 }
 
 def get_destination( cell )
-	MAPS_NUMBERS[cell[1]] * 10 + MAPS_LETTERS[cell[0].upcase]
+	( 8 - cell[1].to_i ) * 10 + MAPS_LETTERS[cell[0].upcase]
+end
+
+def get_cell_names( cell_number )
+	x = cell_number / 10
+	y = cell_number % 10
+	
+	MAPS_LETTERS.key(y).downcase + (8 - x).to_s
 end
 
 def set_H( ending )# 8x8 board. H - distance each cell has from end point
@@ -17,7 +23,7 @@ def set_H( ending )# 8x8 board. H - distance each cell has from end point
 	( 0..7 ).each{ |i| 
 		@h.push []
 		( 0..7 ).each { |j| 
-			@h[i].push ( x - j ).abs + ( y - i ).abs
+			@h[i].push ( x - i ).abs + ( y - j ).abs
 		}
 	}
 	@h
@@ -31,39 +37,81 @@ def get_valid_moves( cell )
 	( 0..incrementals.length - 1 ).step( 2 ).each{ |index|
 		x_move = x + incrementals[index]
 		y_move = y + incrementals[index + 1]
-		valid_moves.push x_move * 10 + y_move		if x_move >= 0 and y_move >= 0
+		valid_moves.push x_move * 10 + y_move		if x_move >= 0 and y_move >= 0 and x_move < 8 and y_move < 8
 	}
 	valid_moves
 end
 
-def find_target( starting, ending, g = 0, open_list = {}, closed_list = {} )
+def get_F( cell, g )
+	x = cell / 10
+	y = cell % 10
+	@h[x][y] + g
+end
+
+def get_lowest_F( open_list = @open_list )
+	lowest = 9999
+	cell = 0
+	open_list.each{ |key, value|
+		if value[1] < lowest
+			lowest = value[1]
+			cell = key
+		end
+	}
+	cell
+end
+
+def get_final_path( cell )
+	list = [cell]
+	cell = @open_list[cell][0]
+	list.push cell
+	g = 9999
+	while( true )
+		list.push @closed_list[cell][0]
+		cell = @closed_list[cell][0]
+		break		if @closed_list[cell] == nil
+		g = @closed_list[cell][1]		
+	end
+	list
+end
+
+def find_target( starting, ending, g = 0 )
 	return		if starting == ending #Base case
 	childs = get_valid_moves starting
 	
 	childs.each{ |cell|
-		if open_list.keys.include? cell
-			open_list[cell] = [ starting, g + 3, get_F( g + 3 )]		if g + 3 < open_list[cell][1]
-		elsif closed_list.include? cell
+		if @open_list.keys.include? cell
+			@open_list[cell] = [ starting, g + 3, get_F( cell, g + 3 )]		if g + 3 < @open_list[cell][1]
+		elsif @closed_list.keys.include? cell
 			childs.delete cell
 		else
-			open_list[cell] = [ starting, g + 3, get_F( g + 3 )]
+			@open_list[cell] = [ starting, g + 3, get_F( cell, g + 3 )]
 		end
 	}
 	
-	open_list.delete starting
-	closed_list.push starting
-	open_list = open_list.sort_by{ |key, values| values[1] }
+	@closed_list[starting] = @open_list[starting]
+	@open_list.delete starting
+	val_with_lowest_F = get_lowest_F 
 	
-	find_target open_list.keys.first, ending, pen_list.values.first[1], open_list, closed_list
+	find_target val_with_lowest_F, ending, @open_list[val_with_lowest_F][1]
 end
 
 
 #prep
+@open_list = {}
+@closed_list = {}
+
 starting = get_destination ARGV[0]
 ending = get_destination ARGV[1]
-
 prohibited = []
 for i in 2..ARGV.length - 1
 	prohibited.push get_destination ARGV[i]
 end
+set_H ending
 find_target starting, ending
+final = get_final_path ending
+proper_final = ""
+final.reverse.each{ |value|
+	proper_final <<  get_cell_names( value ) + ','
+}
+proper_final = proper_final[0..proper_final.length - 2]
+puts proper_final
--- Book - Best of Ruby Quiz/10 - Knight's Travails/tests.rb
+++ Book - Best of Ruby Quiz/10 - Knight's Travails/tests.rb
@@ -1,22 +1,22 @@
 require 'rspec'
 require 'main'
 
-# describe '#get_destination' do
+describe '#get_destination' do
 
-	# context 'passed a value as a chess board reference' do
-		# it 'is provided with a value of A6' do
-			# expect( get_destination 'A6' ).to eq 20
-		# end
+	context 'passed a value as a chess board reference' do
+		it 'is provided with a value of A6' do
+			expect( get_destination 'A6' ).to eq 20
+		end
 		
-		# it 'is provided with a value of E4' do
-			# expect( get_destination 'E4' ).to eq 44
-		# end
+		it 'is provided with a value of E4' do
+			expect( get_destination 'E4' ).to eq 44
+		end
 		
-		# it 'is provided with a value of G5' do
-			# expect( get_destination 'G5' ).to eq 36
-		# end
-	# end
-# end
+		it 'is provided with a value of G5' do
+			expect( get_destination 'G5' ).to eq 36
+		end
+	end
+end
 
 describe '#set_H' do
 
@@ -29,6 +29,40 @@ describe '#set_H' do
 		it 'is provided with endpoint E5 (34)' do
 			arr = set_H 34
 			expect( arr[0][0] ).to eq 7
+		end
+	end
+end
+
+describe '#get_F' do
+
+	context 'provided with a valid cell and g-value' do
+		it 'is provided with 11 and 3' do
+			arr = set_H 20
+			expect( get_F 11, 3 ).to eq 5
+		end
+		
+		it 'is provided with 14 and 6' do
+			arr = set_H 34
+			expect( get_F 14, 6 ).to eq 8
+		end
+	end
+end
+
+describe '#get_lowest_F' do
+
+	context 'provided with a valid list' do
+		it 'returns the value with the lowest F' do
+			open_list = { 34 => [0, 3, 5], 12 => [3, 6, 9] }
+			expect( get_lowest_F open_list ).to eq 34
+		end
+	end
+end
+
+describe '#get_cell_names' do
+
+	context 'provided with a valid cell' do
+		it 'returns the respective cell name (e2)' do
+			expect( get_cell_names 64 ).to eq 'e2'
 		end
 	end
 end
