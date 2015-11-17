b6b0a0a698a6d73629883fc6602a1dc4e7c89b43
Marvin Seguna <mseguna1@gmail.com>
2015-11-17 21:31:22 +0100

    Refactoring
    Divided methods to increase readability

--- Book - Best of Ruby Quiz/12 - Crosswords/print_func.rb
+++ Book - Best of Ruby Quiz/12 - Crosswords/print_func.rb
@@ -1,41 +1,48 @@
-def get_full_printable_crossword( outer, labelled )
+def get_second_line( outer, labelled, i )
 	count = 1
+	line_2 = ''
+	( 0..outer[i].length - 1 ).each do |j|
+		if labelled[i][j] == 'l'
+			line_2 << "##{count}" + " " * ( 5 - "##{count}".length )
+			count += 1
+		else
+			line_2 << outer[i][j].gsub( 'd', '#####' ).gsub( 'x', '#####' ).gsub( '_', '#    ' )
+		end
+	end
+	line_2
+end
+
+def get_full_printable_crossword( outer, labelled )
 	arr = []
 	( 0..outer.length - 1 ).each do |i|
 		arr.push '#' * ( 5 * outer[i].join.length ) + '#' #first line
-		line_2 = ''
-		( 0..outer[i].length - 1 ).each do |j|
-			if labelled[i][j] == 'l'
-				line_2 << "##{count}" + " " * ( 5 - "##{count}".length )
-				count += 1
-			else
-				line_2 << outer[i][j].gsub( 'd', '#####' ).gsub( 'x', '#####' ).gsub( '_', '#    ' )
-			end
-		end
-		arr.push line_2 + '#'
+		arr.push get_second_line( outer, labelled, i ) + '#'
 		arr.push outer[i].join.gsub( 'd', '#####' ).gsub( 'x', '#####' ).gsub( '_', '#    ' ) + '#'
 	end
 	arr.push '#' * ( 5 * outer[0].join.length ) + '#' #last line
 	arr
 end
 
+def handle_d( outer, full_crossword, i, j )
+	x = i * 3
+	y = j * 5
+	fields = 2
+	fields = 3		if i == outer.length - 1
+	
+	for k in 0..fields do
+		next		if k == 0 and i - 1 > 0 and outer[i - 1][j] != 'd'
+	
+		full_crossword[x + k][y] = ' '		if j - 1 < 0 or outer[i][j - 1] == 'd'
+		full_crossword[x + k][y + 1..y + 4] = '    '
+		full_crossword[x + k][y + 5] = ' '		if j == outer[i].length - 1
+	end
+	full_crossword
+end
+
 def get_final_crossword( outer, full_crossword )
 	( 0..outer.length - 1 ).each do |i|
 		( 0..outer[i].length - 1 ).each do |j|
-			if outer[i][j] == 'd'
-				x = i * 3
-				y = j * 5
-				fields = 2
-				fields = 3		if i == outer.length - 1
-				
-				for k in 0..fields do
-					next		if k == 0 and i - 1 > 0 and outer[i - 1][j] != 'd'
-				
-					full_crossword[x + k][y] = ' '		if j - 1 < 0 or outer[i][j - 1] == 'd'
-					full_crossword[x + k][y + 1..y + 4] = '    '
-					full_crossword[x + k][y + 5] = ' '		if j == outer[i].length - 1
-				end
-			end
+			handle_d outer, full_crossword, i, j		if outer[i][j] == 'd'
 		end
 	end
 	full_crossword
