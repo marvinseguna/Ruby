73efb28530074671869018cfff31cd5ca01ff568
Marvin Seguna <mseguna1@gmail.com>
2015-11-21 15:05:03 +0100

    Final Code

--- Book - Best of Ruby Quiz/13 - 1-800-THE-QUIZ/main.rb
+++ Book - Best of Ruby Quiz/13 - 1-800-THE-QUIZ/main.rb
@@ -1,8 +1,6 @@
-#NEED TO CLEAN CODE!!!!!
+require 'set'
 
 def encodings( letter )
-	letter = letter.downcase
-
 	return '2'		if ['a','b','c'].include? letter
 	return '3'		if ['d','e','f'].include? letter
 	return '4'		if ['g','h','i'].include? letter
@@ -15,61 +13,70 @@ end
 
 def encode( word, i = 0 ) #i = index
 	return ''		if i == word.length
-	
 	return encodings( word[i] ) + encode( word, i + 1 )
 end
 
 def get_storage( array )
-	matched = {}
-	for i in 0..array.length - 1
-		matched[i] = []
-	end
-	matched
+	h =  Hash.new{ |hash, key| hash[key] = Set.new } #default value
+	( 0..array.length - 1 ).each{ |index| h[index] }
+	h
 end
 
-def get_possibilities( dictionary, phone )
-	phone_digits = phone.split '.'
+def check_other( word, number, phone_digits )
+	phone_digits.each.with_index{ |value, index| 
+		next		if value.length != number.length
+		insert = true
+		
+		for i in 0..value.length - 2
+			if value[i] != number[i] and value[i + 1] != number[i + 1]
+				insert = false
+				break
+			end
+		end
+		@others[index].add word		if insert
+	}
+end
+
+def process( word, phone_digits )
+	number = encode word
+	index = phone_digits.index number
+	
+	if index != nil #matched
+		@matched[index].add word
+	else
+		check_other word, number, phone_digits
+	end
+end
+
+def read_dictionary( dictionary )
+	phone_digits = ARGV[0].split '.' #873.7829
 	phone_lengths = phone_digits.map{ |digit| digit.length }
-	matched = get_storage phone_digits
-	almost = get_storage phone_digits
 	
 	File.readlines(dictionary).each{ |word| 
-		word = word.chomp
+		word = word.chomp.downcase
 		next		if !phone_lengths.include? word.length #performance #1
-
-		number = encode word
-		if phone_digits.include? number
-			phone_digits.each.with_index{ |value, index| 
-				matched[index].push word		if phone_digits[index] == number and !matched[index].include? word
-			}
-		else
-			phone_digits.each.with_index{ |value, index| 
-				next		if value.length != number.length
-			
-				consecutive = false
-				no_match = false
-				
-				for i in 0..value.length - 1
-					val = value[i]
-					num = number[i]
-					
-					if val != num and consecutive
-						no_match = true
-						break
-					elsif val != num and !consecutive
-						consecutive = true
-					elsif val == num
-						consecutive = false
-					end
-				end
-				almost[index].push word		if !no_match and !almost[index].include? word
-			}
-		end
+		
+		process word, phone_digits
 	}
-	puts "Daw kwazi!!!"
-	puts almost
-	puts "Dawn insta"
-	puts matched
 end
 
-get_possibilities ARGV[2], ARGV[0]
+def print_phone
+	r = Random.new
+	x = ''
+	@matched.each{ |key, value|
+		if !@matched[key].empty?
+			x << "#{@matched[key].to_a[r.rand 0..@matched[key].length - 1]}."
+		else
+			x <<  "#{@others[key].to_a[r.rand 0..@others[key].length - 1]}."
+		end
+	}
+	puts x[0..x.length - 2]
+end
+
+#Hashes which hold the (possible) matches
+@matched = get_storage ARGV[0].split '.'
+@others = get_storage ARGV[0].split '.'
+
+read_dictionary ARGV[2]
+print_phone 
+#sample input: 873.7829 -d Resources/dictionary.txt
