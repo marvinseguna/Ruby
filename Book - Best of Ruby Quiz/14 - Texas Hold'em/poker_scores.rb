5d33e0e1e8cb19f072fbfc13aa84850811588bff
Marvin Seguna <mseguna1@gmail.com>
2015-12-04 17:02:08 +0100

    Re-factored some methods

--- Book - Best of Ruby Quiz/14 - Texas Hold'em/poker_scores.rb
+++ Book - Best of Ruby Quiz/14 - Texas Hold'em/poker_scores.rb
@@ -2,7 +2,7 @@ INDEX_RANKING = { '2' => 0, '3' => 1, '4' => 2, '5' => 3, '6' => 4, '7' => 5, '8
 
 def is_royal_flush( hand )
 	['d', 's', 'c', 'h'].each{ |c|
-		return true => c		if hand.scan(/([a-z]#{c})/i).length == 5
+		return true		if hand.scan(/([tjkqa]#{c})/i).length == 5 #tjkqa = ten jack queen king ace
 	}
 	false
 end
@@ -11,17 +11,14 @@ def is_straight_flush( hand )
 	['d', 's', 'c', 'h'].each{ |c, occur_5 = []|
 		if hand.count( c ) == 5 
 			hand.split.each{ |val| occur_5.push INDEX_RANKING[val[0]]		if val.include? c }
-			return occur_5.sort.each_cons(2).all?{ |a,b| b = a + 1 } => occur_5
+			return occur_5.sort.each_cons(2).all?{ |a,b| b = a + 1 }
 		end
 	}
 	false
 end
 
 def is_four_of_a_kind( hand )
-	INDEX_RANKING.keys.each{ |face| 
-		return true => face		if hand.scan(/#{face}[dsch]/i).length == 4
-	}
-	false
+	same_kind hand, 4
 end
 
 def is_full_house( hand )
@@ -32,34 +29,37 @@ end
 
 def is_flush( hand )
 	['d', 's', 'c', 'h'].each{ |c|
-		return true => c		if hand.scan(/([a-z0-9]#{c})/i).length == 5
+		return true		if hand.scan(/([a-z0-9]#{c})/i).length == 5
 	}
 	false
 end
 
 def is_straight( hand )
+	
 end
 
 def is_three_of_a_kind( hand )
-	INDEX_RANKING.keys.each{ |face| 
-		return true => face		if hand.scan(/#{face}[dsch]/i).length == 3
-	}
-	false
+	same_kind hand, 3
 end
 
 def is_two_pair( hand )
 end
 
 def is_pair( hand )
-	INDEX_RANKING.keys.each{ |face| 
-		return true => face		if hand.scan(/#{face}[dsch]/i).length == 2
-	}
-	false
+	same_kind hand, 2
 end
 
 def is_high_card( hand )
 end
 
-def get_occurence_matches( string, occurrence ) # "Q345QQ7", 3 => "Q", else nil
+#HELPERS
+def same_kind( hand, amount ) # amount => faces that need to be present in hand
+	INDEX_RANKING.keys.each{ |face| 
+		return true		if hand.scan(/#{face}[dsch]/i).length == amount
+	}
+	false
+end
+
+def get_occurence_matches( string, occurrence ) # ( "Q345QQ7", 3 ) => "Q", ( "1234567", 3 ) => nil
 	string.split("").group_by{ |val| val }.select{ |key, value| value.size == occurrence }.map(&:first).first
 end
