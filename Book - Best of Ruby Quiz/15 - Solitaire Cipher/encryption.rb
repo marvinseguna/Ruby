f3378fcbda9210dab930053b98ceca2b166a44a4
Marvin Seguna <mseguna1@gmail.com>
2015-12-08 16:05:16 +0100

    Commit #2
    Renamed file main.rb => keystream.rb
    Added encryption functionality

---
+++ Book - Best of Ruby Quiz/15 - Solitaire Cipher/encryption.rb
@@ -0,0 +1,35 @@
+require 'keystream'
+
+LETTERS_TO_VALUE = { 'A' => 1, 'B' => 2, 'C' => 3, 'D' => 4, 'E' => 5, 'F' => 6, 'G' => 7, 'H' => 8, 'I' => 9, 'J' => 10, 'K' => 11, 'L' => 12, 'M' => 13,
+	'N' => 14, 'O' => 15, 'P' => 16, 'Q' => 17, 'R' => 18, 'S' => 19, 'T' => 20, 'U' => 21, 'V' => 22, 'W' => 23, 'X' => 24, 'Y' => 25, 'Z' => 26 }
+
+def split_sentence( sentence )
+	sentence << 'X'		while sentence.length % 5 != 0
+	sentence.scan(/.{1,5}/).join ' ' #split into chunks of 5
+end
+
+def convert_letters( sentence )
+	sentence.gsub(' ', '').split("").map{ |i| LETTERS_TO_VALUE[i] }
+end
+
+def add_arrays( arr_1, arr_2 )
+	[arr_1, arr_2].transpose.map {|x| ( x.reduce( :+ ) - ( 26 * ( x.reduce( :+ ) / 26 ))) + ( 52 * ( x.reduce( :+ ) / 52 )) }
+end
+
+def get_letters( arr )
+	arr.map{ |val| VALUE_TO_LETTERS[val] }.join.scan(/.{1,5}/).join ' '
+end
+
+def encrypt( sentence )
+	new_sentence = sentence.scan(/([a-z]+)/i).join.upcase #eliminate symbols and upper case remaining
+	
+	splitted = split_sentence new_sentence
+	key = keystream splitted
+	
+	num_orig = convert_letters splitted
+	num_key = convert_letters key #converted both to array of integers
+
+	num_both = add_arrays num_orig, num_key #adding 2 arrays
+
+	get_letters num_both #translate values back to letters
+end
--- Book - Best of Ruby Quiz/15 - Solitaire Cipher/testing.rb
+++ Book - Best of Ruby Quiz/15 - Solitaire Cipher/testing.rb
@@ -1,9 +1,16 @@
 gem 'minitest'
 require 'minitest/autorun'
-require 'main'
+require 'keystream'
+require 'encryption'
 
-describe 'when passed a 10 letter word' do
+describe 'when invoking keystream function with a 10-letter word' do
 	it 'must provide the following sequence: DWJXHYRFDG' do
 		( keystream 'ABCDEFGHTY' ).must_equal 'DWJXHYRFDG'
+	end
+end
+
+describe 'when passing a sentence to the encryption method' do
+	it 'must provide the encrypted version' do
+		( encrypt 'Code in Ruby, live longer!' ).must_equal 'GLNCQ MJAFF FVOMB JIYCB'
 	end
 end
