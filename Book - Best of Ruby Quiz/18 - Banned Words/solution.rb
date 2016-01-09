f6c227d8088eab3ce38809c3f3e99c99019ea7f1
Marvin Seguna <mseguna1@gmail.com>
2016-01-09 12:05:49 +0100

    Challenge Completed

--- Book - Best of Ruby Quiz/18 - Banned Words/solution.rb
+++ Book - Best of Ruby Quiz/18 - Banned Words/solution.rb
@@ -1,21 +1,44 @@
 require 'spam_filter'
 
-dict = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine', 'thirty']
+dict = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twentyone', 'twentytwo', 'twentythree', 'twentyfour', 'twentyfive', 'twentysix', 'twentyseven', 'twentyeight', 'twentynine', 'thirty']
 
 def get_banned_words( list_of_words, banned_words = [] )
-	
+	if list_of_words.size == 1
+		banned_words.push list_of_words[0]
+	else
+		half = ( list_of_words.size / 2.0 ).round
+		
+		left = list_of_words[0..half-1]
+		right = list_of_words[half..list_of_words.size]
+		
+		banned_words = get_banned_words left, banned_words		if !@filter.clean?( left.join ' ' )
+		return banned_words		if @filter.verify banned_words
+		banned_words = get_banned_words right, banned_words		if !@filter.clean?( right.join ' ' )
+	end
+	banned_words
 end
 
-def check_dictionary( dictionary )
+def any?( dictionary ) #checks if whole dicitonary is banned or at least 1 word
 	if @filter.verify dictionary
 		puts "Banned words: #{dictionary}"
 		return false
 	end
-	return true		if !@filter.clean? dictionary.join ' ' 
+	return true		if !@filter.clean?( dictionary.join ' ' )
 	
 	puts 'There are no banned words in the dictionary'
 	return false
 end
 
-@filter = LanguageFilter.new ['three', 'four', 'seventeen', 'twenty-four', 'thirty'] #almost 17% of the words banned - book specifies 10%
-list = get_banned_words		if !check_dictionary dict
+#17%
+@filter = LanguageFilter.new ['three', 'four', 'seventeen', 'twentyfour', 'thirty'] #almost 17% of the words banned - book specifies 10%
+list = get_banned_words dict		if any? dict
+puts "\n---------------------\nBanned Words\n"
+p list
+puts "---------------------\nCalls made: #{@filter::clean_calls}\n---------------------"
+
+#3%
+@filter = LanguageFilter.new ['seventeen'] #almost 17% of the words banned - book specifies 10%
+list = get_banned_words dict		if any? dict
+puts "\n---------------------\nBanned Words\n"
+p list
+puts "---------------------\nCalls made: #{@filter::clean_calls}\n---------------------"
